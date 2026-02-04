b43e7e52259eb79ef35d29a4fbed79c97127f6d5
its-yash0003 <yashgautam2407@gmail.com>
2026-02-04 16:57:59 +0530

    updating first draft

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-function showSection(sectionId) {
+// function showSection(sectionId) {
 
-    var sections = document.getElementsByClassName("content");
+//     var sections = document.getElementsByClassName("content");
 
-    for (var i = 0; i < sections.length; i++) {
-        sections[i].style.display = "none";
+//     for (var i = 0; i < sections.length; i++) {
+//         sections[i].style.display = "none";
+//     }
+
+//     document.getElementById(sectionId).style.display = "block";
+// }
+
+
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+        const topOffset = 80; // height of header
+        const elementPosition = section.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.pageYOffset - topOffset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        });
     }
+}
 
-    document.getElementById(sectionId).style.display = "block";
+function scrollToContact() {
+    const contactSection = document.querySelector('.contact');
+    contactSection.scrollIntoView({ behavior: 'smooth' });
 }
